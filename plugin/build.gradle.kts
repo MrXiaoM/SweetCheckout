cf1141df0fe4db58b7cbfc9b0d2ed22b10171705
MrXiaoM <coolxiaom95@gmail.com>
2026-03-29 17:24:32 +0800

    upgrade dependencies

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 allprojects {
     dependencies {
         if (configurations.findByName("implementation") != null) {
-            add("implementation", "de.tr7zw:item-nbt-api:2.15.5")
+            add("implementation", "de.tr7zw:item-nbt-api:2.15.6")
         }
     }
 }
