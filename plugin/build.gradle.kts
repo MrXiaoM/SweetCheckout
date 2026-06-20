c2049530a59ae3d788b9a869e95a4f9656456281
MrXiaoM <coolxiaom95@gmail.com>
2026-06-20 19:10:04 +0800

    upgrade dependencies

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     repositories.mavenCentral()
-    dependencies.classpath("top.mrxiaom:LibrariesResolver-Gradle:1.7.21")
+    dependencies.classpath("top.mrxiaom:LibrariesResolver-Gradle:1.7.26")
 }
 allprojects {
     val pluginBase = top.mrxiaom.gradle.LibraryHelper(project)
--- plugin/nms/build.gradle.kts
+++ plugin/nms/build.gradle.kts
@@ -1,11 +1,11 @@
 val base: top.mrxiaom.gradle.LibraryHelper by project.extra
 dependencies {
     compileOnly(base.modules.actions)
+    compileOnly(base.depend.annotations)
     compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
-    compileOnly("org.jetbrains:annotations:24.0.0")
     for (proj in subprojects) {
         implementation(proj)
-        proj.dependencies.compileOnly("org.jetbrains:annotations:24.0.0")
+        proj.dependencies.compileOnly(base.depend.annotations)
         if (proj.name != "shared") {
             proj.dependencies.implementation(project(":plugin:nms:shared"))
         }
